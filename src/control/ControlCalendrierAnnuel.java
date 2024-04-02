cd78a05d000a7f540b502b5e86d51c3547625a36
big-gic <ludolacro@gmail.com>
2024-04-02 15:28:44 +0200

    oui

---
+++ src/control/ControlCalendrierAnnuel.java
@@ -0,0 +1,62 @@
+package control;
+
+public class ControlCalendrierAnnuel {
+	private Mois[] calendrier;
+	
+	public ControlCalendrierAnnuel() {
+		calendrier = new Mois[12];
+		calendrier[0] = new Mois("Janvier",31);
+		calendrier[1] = new Mois("Février",28);
+		calendrier[2] = new Mois("Mars",31);
+		calendrier[3] = new Mois("Avril",30);
+		calendrier[4] = new Mois("Mai",31);
+		calendrier[5] = new Mois("Juin",30);
+		calendrier[6] = new Mois("Juillet",31);
+		calendrier[7] = new Mois("Août",31);
+		calendrier[8] = new Mois("Septembre",30);
+		calendrier[9] = new Mois("Octobre",31);
+		calendrier[10] = new Mois("Novembre",30);
+		calendrier[11] = new Mois("Décembre",31);
+	}
+	
+	public boolean estLibre(int jour, int mois) {
+		if (calendrier[mois].estLibre(jour)) {
+			return false;
+		}
+		return true;
+	}
+		
+	public boolean reserver(int jour, int mois) {
+		try{
+			calendrier[mois].reserver(jour);
+		} catch (IllegalStateException e) {
+			return false;
+		}
+		return true;
+	}
+	
+	
+	public class Mois{
+		private String nom;
+		private boolean[] jour;
+		
+		public Mois(String nom, int nbjours) {
+			this.nom = nom;
+			jour = new boolean[nbjours];
+			for (int i=0; i<nbjours; i++) {
+				jour[i] = false;
+			}
+		}
+		
+		public boolean estLibre(int i) {
+			return jour[i];
+		}
+		
+		public void reserver(int i) {
+			if (!estLibre(i)) {
+				throw new IllegalStateException();
+			}
+			jour[i] = true;
+		}
+	}
+}
---
+++ test/control/CalendrierAnnuelTest.java
@@ -0,0 +1,26 @@
+package control;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalendrierAnnuelTest {
+	private ControlCalendrierAnnuel controlCalendrier;
+	
+
+	@Test
+	void testControlCalendrierAnnuel() {
+		
+	}
+
+	@Test
+	void testEstLibre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testReserver() {
+		fail("Not yet implemented");
+	}
+
+}
